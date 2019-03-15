0d0494fa38b603b6272d9b4426d30d6fe21c170c
darco2018 <newsecond@outlook.com>
2019-03-15 15:21:19 +0100

    Display unformatted news items

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,11 @@
 console.log("I'm a great developer");
 
 // Get HTML elements
-let headlinesPara = document.querySelector("p.allheadlines");
+const headlinesPara = document.querySelector("p.allheadlines");
+
+const main = document.querySelector("main");
+let newsItemsArr;
+
 
 
 // Assemble the full URL
@@ -13,16 +17,17 @@ url = baseURL + "top-headlines?" + "country=" + country;
 
 // fetch headlines from bbc
 fetch(url, {
-    headers: {   
+    headers: {
       "x-api-key": bbcKey,
     }
   })
   /* .then(function (response) {
     return response.json(); // implicit conversion to Java Script object
   }) */
-  .then(response => response.json()) 
+  .then(response => response.json())
   .then(function (json) {
-    displayHeadlines(json);
+    //displayHeadlines(json);
+    displayNewsItems(json);
   })
   .catch(function (err) {
     console.log("Fetch problem: " + err.message);
@@ -40,3 +45,75 @@ function displayHeadlines(data) {
   // convert to JS obj ?
   // can write json to file, then to localStorage ?
 }
+
+
+function displayNewsItems(data) {
+
+  newsItemsArr = data["articles"];
+
+  console.log(newsItemsArr);
+
+  for (let i = 0; i < 3; i++) {
+    
+    let item = newsItemsArr[i];
+    // values retrieved from fetched data
+    let title = item.title;
+    let url = item.url;
+    let description = item.description;
+    let imageSrc = item.urlToImage;
+    let published = item.publishedAt;
+    let source = item.source.name;
+
+    /* console.log(title);
+    console.log(url);
+    console.log(description);
+    console.log(imageSrc);
+    console.log(published);
+    console.log(source); */
+
+    let article = document.createElement("article");    
+
+    // header
+    let header = document.createElement("header");
+    let headline = document.createElement("h3");
+    let linkToSource = document.createElement("a");
+    linkToSource.textContent = title;
+    linkToSource.href = url;
+    linkToSource.target = "_blank";
+    headline.appendChild(linkToSource);
+    header.appendChild(headline);
+
+    // section
+    let section = document.createElement("section");
+    let descriptionPara = document.createElement("p");
+    descriptionPara.textContent = description;
+    let img = document.createElement("img");
+    img.src = imageSrc;
+    section.appendChild(descriptionPara);
+    section.appendChild(img);
+
+    //footer
+    let footer = document.createElement("footer");
+    let time = document.createElement("time");
+    time.dateTime = published;
+    time.textContent = published;
+    let sourceSpan = document.createElement("span");
+    sourceSpan.textContent = source;
+    footer.appendChild(time);
+    footer.appendChild(sourceSpan);
+
+    let line = document.createElement("hr");
+
+    // assemble article components 
+    main.appendChild(article);   
+    article.appendChild(header);
+    article.appendChild(section);
+    article.appendChild(footer);   
+    main.appendChild(line);
+
+  }
+}
+
+
+
+//console.log(Array.isArray(data)); // false
